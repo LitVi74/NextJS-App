a94d33fe6a57d1900dd3173bbac40658ae269974
LitVi74 <71370749+LitVi74@users.noreply.github.com>
2023-02-20 21:04:30 +0500

    edit index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,33 @@
 import MainContainer from "../components/Main.Container";
+import {IPost} from "../types/posts";
 
-const Index = () => {
+interface IIndex {
+    posts: IPost[];
+}
+
+const Index = ({posts}: IIndex) => {
     return (
         <MainContainer>
-            <div>
-                Home Page
-            </div>
+            <>
+               <h1>Posts Page</h1>
+                <ul>
+                    {posts.map(post =>
+                    <li key={post.id}>
+                        {post.title}
+                    </li>
+                    )}
+                </ul>
+            </>
         </MainContainer>
     );
 };
 
 export default Index;
+
+export async function getStaticProps(context) {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await response.json() as IPost[];
+    return {
+        props: {posts}, // will be passed to the page component as props
+    }
+}
