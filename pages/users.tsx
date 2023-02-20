8e8ea2a4f8a40dff04e47a85c2768c9021d68119
LitVi74 <71370749+LitVi74@users.noreply.github.com>
2023-02-20 22:21:37 +0500

    add user list

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
 import MainContainer from "../components/Main.Container";
+import {IUser} from "../types/users";
+import Link from "next/link";
 
-const Users = () => {
+interface IUsersComponent {
+    users: IUser[];
+}
+
+const Users = ({users}: IUsersComponent) => {
     return (
         <MainContainer>
-            <div>
-                Users Page
-            </div>
+            <>
+                <h1>
+                    Users Page
+                </h1>
+                <ul>
+                    {users.map(user =>
+                    <li key={user.id}>
+                        <Link href={`/users/${user.id}`}>
+                            {user.username}
+                        </Link>
+                    </li>
+                    )}
+                </ul>
+            </>
         </MainContainer>
     );
 };
 
 export default Users;
+
+export async function getStaticProps(context) {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await response.json() as IUser[];
+    return {
+        props: {users}, // will be passed to the page component as props
+    }
+}
