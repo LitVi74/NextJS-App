7ec30802de10519573350782a86dde365a861ef5
LitVi74 <71370749+LitVi74@users.noreply.github.com>
2023-02-20 20:15:39 +0500

    add users page

---
+++ pages/users.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainContainer from "../components/Main.Container";
+
+const Users = () => {
+    return (
+        <MainContainer>
+            <div>
+                Users Page
+            </div>
+        </MainContainer>
+    );
+};
+
+export default Users;
