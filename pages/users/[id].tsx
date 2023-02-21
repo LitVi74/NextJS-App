8aadf72807f130bc325f6e1e9e3161fb3b222bac
LitVi74 <71370749+LitVi74@users.noreply.github.com>
2023-02-21 20:27:08 +0500

    add user page

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,3 +1,41 @@
-export default function User () {
-    
+import MainContainer from "../../components/Main.Container";
+import {IUser} from "../../types/users";
+import {IPost} from "../../types/posts";
+import Link from "next/link";
+
+interface IUserComponent {
+    user: IUser;
+    userPosts: IPost[]
+}
+
+export default function User ({user, userPosts}: IUserComponent) {
+    return (
+        <MainContainer>
+            <>
+                <h1>{user.username}</h1>
+                <p>User full name: {user.name}</p>
+                <ul>
+                    {userPosts.map(post =>
+                    <li>
+                        <Link href={`/posts/${post.id}`}>
+                            {post.title}
+                        </Link>
+                    </li>
+                    )}
+                </ul>
+            </>
+        </MainContainer>
+    )
+}
+
+export async function getServerSideProps({params}) {
+    const response = await Promise.all([
+        fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`),
+        fetch(`https://jsonplaceholder.typicode.com/posts?userId=${params.id}`)
+    ])
+    const user = await response[0].json() as IUser;
+    const userPosts = await response[1].json() as IPost[];
+    return {
+        props: {user, userPosts}, // will be passed to the page component as props
+    }
 }
